2e1361e59e861e56d4e2627dfd20adaed9775a14
SydFrank <frankzhsy@gmail.com>
2025-07-06 20:29:55 +1000

    Router Component

---
+++ dashboard/src/router/Router.jsx
@@ -0,0 +1,18 @@
+/**
+ * Router Component
+ *
+ * This component takes a list of route configurations (allRoutes)
+ * and renders the matched component using React Router's useRoutes() hook.
+ */
+import React from "react";
+import { useRoutes } from "react-router-dom";
+
+const Router = ({ allRoutes }) => {
+  // Generate route elements based on route configuration
+  const routes = useRoutes([...allRoutes]); // Clone the array to avoid the side effects
+
+  // Render the matched route element
+  return routes;
+};
+
+export default Router;
