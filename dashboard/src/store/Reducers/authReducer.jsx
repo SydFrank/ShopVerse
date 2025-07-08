b4d4e28876c25bcb1af4f6d785e553c58929f179
SydFrank <frankzhsy@gmail.com>
2025-07-08 20:11:42 +1000

    update port

--- dashboard/src/store/Reducers/authReducer.jsx
+++ dashboard/src/store/Reducers/authReducer.jsx
@@ -1,4 +1,5 @@
 import { createAsyncThunk, createSlice } from "@reduxjs/toolkit";
+import api from "./../../api/api";
 
 /**
  * Async Thunk - Admin Login
@@ -10,12 +11,12 @@ import { createAsyncThunk, createSlice } from "@reduxjs/toolkit";
 export const admin_login = createAsyncThunk(
   "auth/admin_login",
   async (info) => {
-    console.log(info);
+    // console.log(info);
     try {
-      // const data = await api.post("/admin-login", info, {
-      //   withCredentials: true,
-      // });
-      // console.log(data);
+      const data = await api.post("/admin-login", info, {
+        withCredentials: true,
+      });
+      console.log(data);
     } catch (error) {}
   }
 );
