184e550aa6528e452c293f3f77e651edba4eecd1
SydFrank <frankzhsy@gmail.com>
2025-07-14 01:37:13 +1000

    get-user function

--- backend/controllers/authControllers.js
+++ backend/controllers/authControllers.js
@@ -58,9 +58,23 @@ class authControllers {
       responseReturn(res, 500, { error: error.message });
     }
   };
+  // End of admin_login method
+
+  getUser = async (req, res) => {
+    const { id, role } = req;
+    try {
+      if (role === "admin") {
+        const user = await adminModel.findById(id);
+        responseReturn(res, 200, { userInfo: user });
+      } else {
+        console.log("Seller Info");
+      }
+    } catch (error) {
+      console.log(error.message);
+    }
+  };
+  // End of getUser method
 }
 
 // Export an instance of the authControllers class for use in routing
 module.exports = new authControllers();
-
-// End Method
--- backend/routes/authRoutes.js
+++ backend/routes/authRoutes.js
@@ -1,5 +1,6 @@
 // Import the authentication controller module
 const authControllers = require("../controllers/authControllers");
+const { authMiddleware } = require("../middlewares/authMiddleware");
 
 // Create a new Express Router instance to define route handlers
 const router = require("express").Router();
@@ -7,5 +8,7 @@ const router = require("express").Router();
 // Define a POST route at '/admin-login' that uses the admin_login method from authControllers
 router.post("/admin-login", authControllers.admin_login);
 
+// Define a GET route at '/get-user' that uses the authMiddleware for authentication
+router.get("/get-user", authMiddleware, authControllers.get_user);
 // Export the router to be used in the main server application
 module.exports = router;
