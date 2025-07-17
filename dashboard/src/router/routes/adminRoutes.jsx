d8603b9b3d86c8e6111c479526ed8898592ff89c
SydFrank <frankzhsy@gmail.com>
2025-07-17 14:15:09 +1000

    Admin Order Page Design part 2

--- dashboard/src/router/routes/adminRoutes.jsx
+++ dashboard/src/router/routes/adminRoutes.jsx
@@ -1,11 +1,17 @@
 import { lazy } from "react";
 
 const AdminDashboard = lazy(() => import("../../views/admin/adminDashboard"));
+const Orders = lazy(() => import("../../views/admin/Orders"));
 
 export const adminRoutes = [
   {
     path: "admin/dashboard", // Admin dashboard path
     element: <AdminDashboard />, // Admin dashboard component
+    role: "admin", // Accessible by admin role only
+  },
+  {
+    path: "admin/dashboard/orders", // Admin orders management path
+    element: <Orders />, // Admin orders component
     role: "admin", // Accessible by admin role only
   },
 ];
---
+++ dashboard/src/views/admin/Orders.jsx
@@ -0,0 +1,32 @@
+import React from "react";
+
+const Orders = () => {
+  const [currentPage, setCurrentPage] = React.useState(1);
+  const [searchValue, setSearchValue] = React.useState("");
+  const [parPage, setParPage] = React.useState(5);
+
+  return (
+    <div className="px-2 lg:px-7 pt-5">
+      <div className="w-full p-4 bg-[#6a5fdf] rounded-md">
+        <div className="flex justify-between items-center">
+          <select
+            onChange={(e) => setParPage(parseInt(e.target.value))}
+            className="px-4 py-2 focus:border-indigo-500 outline-none bg-[#6a5fdf] border border-slate-700 rounded-md text-[#d0d2d6]"
+          >
+            <option value="5">5</option>
+            <option value="10">10</option>
+            <option value="15">15</option>
+          </select>
+          <input
+            type="text"
+            placeholder="Search"
+            className="px-4 py-2 focus:border-indigo-500 outline-none bg-[#6a5fdf] border border-slate-700 rounded-md text-[#d0d2d6]"
+          />
+        </div>
+      </div>
+      <h1>Orders</h1>
+    </div>
+  );
+};
+
+export default Orders;
