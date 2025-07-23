707b2027cd199eba6d585c06e03f557b3c6bac93
SydFrank <frankzhsy@gmail.com>
2025-07-23 11:26:24 +1000

    Seller Chat Page Design part 3 completed

--- dashboard/src/views/admin/ChatSeller.jsx
+++ dashboard/src/views/admin/ChatSeller.jsx
@@ -1,8 +1,10 @@
 import React, { useState } from "react";
+import { FaList } from "react-icons/fa";
 import { IoMdClose } from "react-icons/io";
 
 const ChatSeller = () => {
   const [show, setShow] = useState(false);
+  const sellerId = 65; // Example seller ID, replace with actual logic to get seller ID
 
   return (
     <div className="px-2 lg:px-7 py-5">
@@ -17,14 +19,15 @@ const ChatSeller = () => {
               <div className="flex text-xl justify-between items-center p-4 md:p-0 md:px-3 md:pb-3 text-white">
                 <h1>Sellers</h1>
                 <span
-                  onClick={() => setShow(true)}
+                  onClick={() => setShow(!show)}
                   className="block cursor-pointer md:hidden"
                 >
                   <IoMdClose />
                 </span>
               </div>
+
               <div
-                className={`h-[60px] flex justify-start gap-2 items-center text-white px-2 py-2 rounded-sm cursor-pointer`}
+                className={`h-[60px] flex justify-start gap-2 items-center text-white px-2 py-2 rounded-md cursor-pointer bg-[#8288ed]`}
               >
                 <div className="relative">
                   <img
@@ -39,6 +42,7 @@ const ChatSeller = () => {
                   </div>
                 </div>
               </div>
+
               <div
                 className={`h-[60px] flex justify-start gap-2 items-center text-white px-2 py-2 rounded-sm cursor-pointer`}
               >
@@ -55,6 +59,7 @@ const ChatSeller = () => {
                   </div>
                 </div>
               </div>
+
               <div
                 className={`h-[60px] flex justify-start gap-2 items-center text-white px-2 py-2 rounded-sm cursor-pointer`}
               >
@@ -70,6 +75,31 @@ const ChatSeller = () => {
                     <h2 className="text-base font-semibold">Amy Wu</h2>
                   </div>
                 </div>
+              </div>
+            </div>
+          </div>
+
+          <div className="w-full md:w-[calc(100%-200px)] md:pl-4">
+            <div className="flex justify-between items-center">
+              {sellerId && (
+                <div className="flex justify-start items-center gap-3">
+                  <div className="relative">
+                    <img
+                      className="w-[45px] h-[45px] border-green-500 border-2 max-w-[45px] p-[2px] rounded-full"
+                      src="/images/demo.jpg"
+                    />
+                    <div className="w-[10px] h-[10px] bg-green-500 rounded-full absolute right-0 bottom-0"></div>
+                  </div>
+                </div>
+              )}
+
+              <div
+                onClick={() => setShow(!show)}
+                className="w-[35px] flex md:hidden h-[35px] rounded-sm bg-blue-500 shadow-lg hover:shadow-blue-500/50 cursor-pointer justify-center items-center text-white"
+              >
+                <span>
+                  <FaList />
+                </span>
               </div>
             </div>
           </div>
