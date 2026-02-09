dc09b36fbf0476c4781edd82d30bf86e750034e9
SydFrank <frankzhsy@gmail.com>
2026-02-09 14:01:57 +1100

    deploy update 9

--- dashboard/src/utils/utils.jsx
+++ dashboard/src/utils/utils.jsx
@@ -11,7 +11,7 @@ export const overrideStyle = {
 
 import { io } from "socket.io-client";
 
-export const socket = io("https://backend-shopverse-version-2.onrender.com", {
+export const socket = io("https://backend-shopverse-version-3.onrender.com", {
   withCredentials: true,
   transports: ["websocket", "polling"],
 });
