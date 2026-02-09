19e4834a448c88ed3177feb8dd095de8e927b1ae
SydFrank <frankzhsy@gmail.com>
2026-02-09 22:31:03 +1100

    deploy update 14

--- dashboard/src/api/api.js
+++ dashboard/src/api/api.js
@@ -28,8 +28,6 @@ import axios from "axios"; // Popular HTTP client library for making API request
 const local = "http://localhost:5000";
 
 // Production server URL - update this with actual production domain
-// Example: "https://api.shopverse.com" or "https://your-domain.com"
-
 const production = "https://backend-shopverse-version-4-update.onrender.com";
 // Variable to hold the selected API base URL based on the environment
 let api_url = "";
